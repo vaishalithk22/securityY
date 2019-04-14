0c8942f608691ec5409a1439e0ecc637de22e89b
vaishalithk22 <vaishalithk22@gmail.com>
2019-04-14 14:11:04 +0530

    deleting jwt

--- jwt-security-app/src/main/java/com/securex/api/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.securex.api.service;
-
-import com.securex.api.model.admindispatcher.Administrator;
-import com.securex.api.model.admindispatcher.AdminDto;
-
-public interface AdminService 
-{
-	Administrator save(AdminDto admin);
-}
